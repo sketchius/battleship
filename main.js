458817f98207b288124f0486efe63b3112aa0eaa
Bryce Huhtala <sketchius@gmail.com>
2022-07-30 11:53:42 -0400

    Implement game board object, integrate with ship object

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-function shipFactory (x,y,orientation,length) {
+function shipFactory (x,y,rotation,length) {
 
     const hullHit = [];
     for (let i = 0; i < length; i++) {
         hullHit[i] = false;
     }
 
-    const hit = (position) => { hullHit[position] = true; }
+    const registerHit = (position) => { hullHit[position] = true; }
 
     const isSunk = () => {
         for (let i = 0; i < length; i++) {
@@ -14,8 +14,103 @@ function shipFactory (x,y,orientation,length) {
         return true;
     }
 
-    return { isSunk, hit };
+    return { isSunk, registerHit, x, y, rotation, length };
 };
 
 
-export { shipFactory };
+
+function boardFactory (width, height) {
+    const ships = [];
+    const grid = [];
+    for (let x = 0; x < width; x++) {
+        grid[x] = [];
+        for (let y = 0; y < height; y++) {
+            grid[x][y] = 'open';
+        }
+    }
+    
+    const placeShip = (x,y,rotation,length) => {
+        let occupied = false;
+        if (rotation == 0) {
+            for (let i = 0; i < length; i++) {
+                const result = getShipAtPoint(x+i,y);
+                if (result.occupied) {
+                    occupied = true;
+                }
+            }
+        } else {
+            for (let i = 0; i < length; i++) {
+                const result = getShipAtPoint(x,y+i);
+                if (result.occupied) {
+                    occupied = true;
+                }
+            }
+        }
+        if (occupied) { return false; }
+        const ship = shipFactory(x,y,rotation,length);
+        ships.push(ship);
+        return true;
+    }
+
+    const getShipAtPoint = (x,y) => {
+        let occupied = false;
+        let shipObject;
+        let position = 0;
+        ships.forEach( (ship) => {
+            if (ship.rotation == 0) {
+                for (let i = 0; i < ship.length; i++) {
+                    if (x == ship.x + i && y == ship.y) {
+                        occupied = true;
+                        shipObject = ship;
+                        position = i;
+                    }
+                }
+            } else {
+                for (let i = 0; i < ship.length; i++) {
+                    if (x == ship.x && y == ship.y + i) {
+                        occupied = true;
+                        shipObject = ship;
+                        position = i;
+                    }
+                }
+            }
+        })
+        return { occupied, ship: shipObject, position };
+    }
+
+    const recieveAttack = (x,y) => {
+        if (x < 0 || y < 0 || x >= width || y >= height) {
+            return 'invalid';
+        }
+        switch (grid[x][y]) {
+            case 'open':
+                const result = getShipAtPoint(x,y);
+                if (result.occupied) {
+                    grid[x][y] = 'hit';
+                    if (result.ship != null) {
+                        result.ship.registerHit(result.position);
+                        if (result.ship.isSunk()) {
+                            return 'sunk';
+                        } else {
+                            return 'hit';
+                        }
+                    } else {
+                        return 'error'
+                    }
+                } else {
+                    grid[x][y] = 'miss';
+                    return 'miss';
+                }
+                break;
+            case 'miss':
+            case 'hit':
+            case 'sunk':
+                return 'invalid';
+        }
+    }
+    
+    return { placeShip, recieveAttack }
+};
+
+
+export { shipFactory, boardFactory };
--- main.test.js
+++ main.test.js
@@ -1,4 +1,4 @@
-import { shipFactory } from './main.js';
+import { shipFactory, boardFactory } from './main.js';
 
 describe('Ship Object Tests', () => {
     it('isSunk() happy path', () => {
@@ -16,5 +16,72 @@ describe('Ship Object Tests', () => {
         ship.registerHit(1);
         ship.registerHit(2);
         expect(ship.isSunk()).toBe(true);
+    });
+    it('Coordinates test', () => {
+        const ship = shipFactory(6,4,0,3);
+        expect(ship.x).toBe(6);
+    });
+    it('Orientation test', () => {
+        const ship = shipFactory(0,0,1,3);
+        expect(ship.rotation).toBe(1);
+    });
+    it('Length test', () => {
+        const ship = shipFactory(0,0,1,3);
+        expect(ship.length).toBe(3);
+    });
+});
+
+describe('Gameboard Object Tests', () => {
+    it('Placement works', () => {
+        const gameBoard = boardFactory(10,10);
+        expect(gameBoard.placeShip(3,3,0,3)).toBe(true);
+    });
+    it('Overlapping placement fails', () => {
+        const gameBoard = boardFactory(10,10);
+        gameBoard.placeShip(3,3,0,3);
+        expect(gameBoard.placeShip(3,3,0,3)).toBe(false);
+    });
+    it('Overlapping placement fails (+ overlap)', () => {
+        const gameBoard = boardFactory(10,10);
+        gameBoard.placeShip(3,3,0,3);
+        expect(gameBoard.placeShip(4,2,1,3)).toBe(false);
+    });
+    it('Basic attack hit test', () => {
+        const gameBoard = boardFactory(10,10);
+        gameBoard.placeShip(3,3,0,3);
+        expect(gameBoard.recieveAttack(3,3)).toBe('hit');
+    });
+    it('Vertical rotation attack hit test', () => {
+        const gameBoard = boardFactory(10,10);
+        gameBoard.placeShip(4,2,1,3);
+        expect(gameBoard.recieveAttack(4,3)).toBe('hit');
+    });
+    it('Basic attack miss test', () => {
+        const gameBoard = boardFactory(10,10);
+        gameBoard.placeShip(3,3,0,3);
+        expect(gameBoard.recieveAttack(3,4)).toBe('miss');
+    });
+    it('Sink ship test', () => {
+        const gameBoard = boardFactory(10,10);
+        gameBoard.placeShip(3,3,0,2);
+        gameBoard.recieveAttack(3,3);
+        expect(gameBoard.recieveAttack(4,3)).toBe('sunk');
+    });
+    it('Double miss test', () => {
+        const gameBoard = boardFactory(10,10);
+        gameBoard.placeShip(3,3,0,3);
+        gameBoard.recieveAttack(5,5);
+        expect(gameBoard.recieveAttack(5,5)).toBe('invalid');
+    });
+    it('Double hit test', () => {
+        const gameBoard = boardFactory(10,10);
+        gameBoard.placeShip(3,3,0,3);
+        gameBoard.recieveAttack(3,3);
+        expect(gameBoard.recieveAttack(3,3)).toBe('invalid');
+    });
+    it('Out of bounds test', () => {
+        const gameBoard = boardFactory(10,10);
+        gameBoard.placeShip(3,3,0,3);
+        expect(gameBoard.recieveAttack(12,12)).toBe('invalid');
     });
 });
